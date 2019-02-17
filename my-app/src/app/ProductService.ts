fa135517d1163f5cdc868c9d0bf06f6d4760d553
minhnt303 <minhnt303@gmail.com>
2019-02-17 14:08:36 +0700

    commit

--- my-app/src/app/ProductService.ts
+++ my-app/src/app/ProductService.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import 'Rxjs/Rx';
-import 'rxjs/Rx';
+import { map } from 'rxjs/operators';
 // import { User } from './login/login.component';
 import { Headers, RequestOptions } from '@angular/http';
 @Injectable()
@@ -11,7 +10,7 @@ export class ProductService {
   ) {}
 
   getProduct() {
-    return this.http.get('http://localhost:54934/api/Product/Get').map((res:Response) => res.json());
+    return this.http.get('http://localhost:54934/api/Product/Get').pipe(map((res:Response) => res.json()));
   }
   // postProduct(Product: Product) {
   //   let headers = new Headers({ 'Content-Type': 'application/json' });
